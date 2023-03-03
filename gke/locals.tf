924ecc2fd89701822a354dd62dbce50ca5113b3e
Zhakyp <73534500+Zhakyp01@users.noreply.github.com>
2023-03-03 10:15:52 +0600

    Update locals.tf

--- gke/locals.tf
+++ gke/locals.tf
@@ -15,7 +15,7 @@ locals {
   }
 
   location = {
-    "first-node-pool"  = "us-west1",
-    "second-node-pool" = "us-west1",
+    "first-node-pool"  = "us-central1",
+    "second-node-pool" = "us-central1",
   }
 }
